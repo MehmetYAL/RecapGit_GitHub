9aa6022a50e9e74cb59e0953d1ffed806524e8d0
Mehmet Yalcin <mehmetyalcin@hotmail.com>
2022-12-06 20:58:56 +0300

    second updated

--- src/test/HelloTest.java
+++ src/test/HelloTest.java
@@ -3,5 +3,6 @@ package test;
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("hello test");
+        System.out.println("will make share project on git hub");
     }
 }
